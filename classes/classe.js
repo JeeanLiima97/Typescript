7dda93b10563d79160fb48ea0bc41cf766f9c89c
Jean Lima de Souza Junior <jean.jr.souza@hotmail.com>
2020-07-20 16:27:31 -0300

    Utilizando Get e set

--- classes/classe.js
+++ classes/classe.js
@@ -15,6 +15,7 @@ console.log(aniversario);
 var casamento = new Data; //posso omitor os "()"
 casamento.ano = 2017;
 console.log(casamento);
+///--------------------------------------------------------------------------------------------///
 var DataEsperta = /** @class */ (function () {
     function DataEsperta(dia, mes, ano) {
         if (dia === void 0) { dia = 1; }
@@ -32,3 +33,42 @@ var casamentoEsperto = new DataEsperta;
 //posso omitor os "()"
 casamento.ano = 2017;
 console.log(casamentoEsperto);
+// Desafio classe produto
+var Produto = /** @class */ (function () {
+    function Produto(nome, preco, desconto) {
+        if (desconto === void 0) { desconto = 0; }
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+    Produto.prototype.resumo = function () {
+        return this.nome + " custa R$" + this.preco;
+    };
+    return Produto;
+}());
+var p1 = new Produto("Celular", 150);
+var p2 = new Produto('Notebook', 1230);
+var p3 = new Produto('Tv', 1000, 0.1);
+console.log(p1.resumo());
+// Getters & Setters
+var Pessoa = /** @class */ (function () {
+    function Pessoa() {
+        this._idade = 0;
+    }
+    Object.defineProperty(Pessoa.prototype, "idade", {
+        get: function () {
+            return this._idade;
+        },
+        set: function (valor) {
+            if (valor >= 0 && valor <= 100) {
+                this._idade = valor;
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Pessoa;
+}());
+var pe1 = new Pessoa;
+pe1.idade = 10;
+console.log(pe1);
--- classes/classe.ts
+++ classes/classe.ts
@@ -19,7 +19,7 @@ const casamento = new Data //posso omitor os "()"
 casamento.ano = 2017
 console.log(casamento);
 
-
+///--------------------------------------------------------------------------------------------///
 
 
 class DataEsperta{
@@ -36,3 +36,44 @@ const casamentoEsperto = new DataEsperta
  //posso omitor os "()"
 casamento.ano = 2017
 console.log(casamentoEsperto);
+
+
+// Desafio classe produto
+
+class Produto{
+
+    constructor(public nome: string, public preco: number, public desconto: number = 0){
+
+    }
+   public resumo(): string{
+       return `${this.nome} custa R$${this.preco}`
+   }
+
+}
+
+const p1 = new Produto("Celular", 150)
+const p2 = new Produto('Notebook', 1230)
+const p3 = new Produto('Tv', 1000, 0.1)
+console.log(p1.resumo());
+
+
+
+// Getters & Setters
+class Pessoa{
+    private _idade: number = 0
+
+    get idade():number{
+        return this._idade
+    }
+
+    set idade(valor: number){
+        if(valor>= 0 && valor <= 100){
+            this._idade = valor
+        }
+    }
+}
+
+const pe1 = new Pessoa
+pe1.idade = 10
+console.log(pe1);
+
